b0b26ffc46a36584a84c408e15c651a91f4b9b5a
paula.nascimento <paula.nascimento@zup.com.br>
2023-07-07 16:02:05 -0300

    enviando exercícios 1 e 3 do grupoR

---
+++ ExerciciosUsandoNull/src/main/kotlin/grupoR/exercicio01/Main.kt
@@ -0,0 +1,19 @@
+package grupoR.exercicio01
+
+fun main() {
+
+    var nome:String
+    do{
+        print("Digite o seu nome: ")
+        nome = readln()
+    } while (nome.isBlank())
+
+
+    print("Digite a sua idade: ")
+    val idade = readln().toIntOrNull()
+
+
+    val usuario = Pessoa(nome, idade)
+    usuario.informacoes()
+    usuario.maiorDeIdade()
+}
