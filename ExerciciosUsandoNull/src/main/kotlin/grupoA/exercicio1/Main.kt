82c5d7a254b5ccfd4e29690efa0a58e057b93fff
paula.nascimento <paula.nascimento@zup.com.br>
2023-07-07 17:47:08 -0300

    corrigindo exercício 1, grupoA

--- ExerciciosUsandoNull/src/main/kotlin/grupoA/exercicio1/Main.kt
+++ ExerciciosUsandoNull/src/main/kotlin/grupoA/exercicio1/Main.kt
@@ -1,41 +1,42 @@
 package grupoA.exercicio1
 
 fun main() {
-    println("Digite o numero de funcionário")
+    println("Digite o numero de funcionários: ")
     val quantidadeFuncionario = readln().toIntOrNull()
 
     val gradeFuncionario = GradeFuncionario()
     if (quantidadeFuncionario != null && quantidadeFuncionario > 0){
-
         gradeFuncionario.preencherGrade(quantidadeFuncionario)
         gradeFuncionario.exibirFuncionario()
+
+        println("== Média salarial ==")
         gradeFuncionario.calcularMediaSalario()
     }else{
         println("Quantidade inválida")
     }
 }
-class GradeFuncionario{
-
-    data class grade(var nome : String, var cargo : String,var  salario : Double)
-
+class GradeFuncionario(){
+    data class grade(var nome : String,var cargo : String,var salario : Double? = null)
     val listaFuncionario = mutableListOf<grade>()
 
     fun preencherGrade (quantidadeFuncionario : Int) {
 
         for (i in 1..quantidadeFuncionario) {
-            println("Digite o nome do funcionario $i")
+            println("Digite o nome do funcionario $i:")
             val name = readln()
-            println("Digite o cargo $i")
+            println("Digite o cargo do funcionário $i:")
             val cargo = readln()
 
-            println("Digite o salario do funcionario $i")
-            val salario = readln().toDoubleOrNull()
-
-            if (salario != null && salario > 0) {
-                val funcionario = grade(name, cargo, salario)
-                listaFuncionario.add(funcionario)
-            } else {
-                println("Salário inválido o valor deve ser maior que 0")
+            while (true){
+                println("Digite o salario do funcionario $i")
+                val salario = readln().toDoubleOrNull()?:0.0
+                if (salario > 0) {
+                    val funcionario = grade(name, cargo, salario)
+                    listaFuncionario.add(funcionario)
+                    break
+                } else {
+                    println("Salário inválido o valor deve ser maior que 0")
+                }
             }
         }
     }
@@ -43,7 +44,7 @@ class GradeFuncionario{
         var totalSalario = 0.0
 
         for (funcionario in listaFuncionario) {
-            totalSalario += funcionario.salario
+            totalSalario += funcionario.salario!!
         }
         if (listaFuncionario.isNotEmpty()){
             val mediaSalarial= totalSalario/ listaFuncionario.size
@@ -54,9 +55,9 @@ class GradeFuncionario{
         }
     }
     fun exibirFuncionario(){
-
+        println("Funcionários existentes")
         listaFuncionario.forEach{
-            println("Nome: ${it.nome} Cargo: ${it.cargo} Salario:${it.salario}")
+            println("Nome: ${it.nome} /Cargo: ${it.cargo} /Salario:${it.salario}")
         }
 
     }
