0a879c73fec9268ce14ddcf2139aedc5b1877d37
paula.nascimento <paula.nascimento@zup.com.br>
2023-07-07 17:23:19 -0300

    corrigindo exercício 4, grupoA

--- exerciciosUsandoNull/src/main/kotlin/grupoA/exercicio4/Pesquisa.kt
+++ exerciciosUsandoNull/src/main/kotlin/grupoA/exercicio4/Pesquisa.kt
@@ -4,21 +4,24 @@ fun main() {
     var votosMatrix = 0
     var votosSenhorDosAneis = 0
 
-    for (i in 1..2) {
-        println("Opções de sagas concorrentes:\n" +
-                "1. Matrix\n" +
-                "2. Senhor dos Anéis")
-        println("Digite o número correspondente à melhor saga de filme (1 ou 2):")
+    for (i in 1..20) {
+        var votoSaga:Int?
+        do{
+            println("Opções de sagas concorrentes:\n" +
+                    "1. Matrix\n" +
+                    "2. Senhor dos Anéis")
+            println("Digite o número correspondente à melhor saga de filme (1 ou 2):")
 
-        val votoSaga = readlnOrNull()?.toIntOrNull()
+            votoSaga = readlnOrNull()?.toIntOrNull()
 
-        when (votoSaga) {
-            1 -> votosMatrix++
-            2 -> votosSenhorDosAneis++
-            else -> {
-                println("Opção invalida, insira novamente a opção correta")
+            when (votoSaga) {
+                1 -> votosMatrix++
+                2 -> votosSenhorDosAneis++
+                else -> {
+                    println("Opção invalida, insira novamente a opção correta")
+                }
             }
-        }
+        }while (votoSaga == null)
     }
 
     println("Total de votos para a saga Matrix: $votosMatrix")
